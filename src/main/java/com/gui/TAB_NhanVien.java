754400f37eaa2323e669a505d7d3b85f8d8ebe9c
unkluco <23693221.kiet@student.iuh.edu.vn>
2025-11-07 16:16:03 +0700

    fix: minor fĩ

--- src/main/java/com/gui/TAB_NhanVien.java
+++ src/main/java/com/gui/TAB_NhanVien.java
@@ -1,4 +1,16 @@
 package com.gui;
 
-public class TAB_NhanVien {
+import javax.swing.*;
+import java.awt.*;
+
+public class TAB_NhanVien extends JPanel {
+
+    public TAB_NhanVien() {
+        setLayout(new BorderLayout());
+        setBackground(Color.WHITE);
+
+        JLabel lblTitle = new JLabel("TAB HƯỚNG DẪN", SwingConstants.CENTER);
+        lblTitle.setFont(new Font("Segoe UI", Font.BOLD, 24));
+        add(lblTitle, BorderLayout.CENTER);
+    }
 }
